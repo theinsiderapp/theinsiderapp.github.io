b99766d557b6c9baaacd93b15768f87a154ebf93
Ashish Vaid <ashish.vaid@soa.com>
2014-09-26 11:23:40 -0700

    add support for fullpage.js

--- js/default.js
+++ js/default.js
@@ -2,10 +2,38 @@
  * Created by avaid on 9/8/14.
  */
 
-var s = skrollr.init({
-    render: function(data) {
+var insiderSkrollr = skrollr.init({
+    render: function (data) {
         //Debugging - Log the current scroll position.
         //console.log(data.curTop);
     }
 });
-s.refresh($('.slide'));
+
+
+$('#preload').imagesLoaded(function (instance) {
+    setTimeout(function () {
+
+        // Resize sections
+        adjustWindow();
+
+        // Fade in sections
+        //$body.removeClass('loading').addClass('loaded');
+
+    }, 800);
+});
+
+function adjustWindow() {
+    // Get window size
+    var winH = $(window).height();
+
+    // Keep minimum height 550
+    if (winH <= 550) {
+        winH = 550;
+    }
+
+    // Resize our slides
+    $('.slide').height(winH);
+
+    // Refresh Skrollr after resizing our sections
+    insiderSkrollr.refresh($('.slide'));
+}
