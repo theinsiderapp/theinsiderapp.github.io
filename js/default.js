ee160d89f33b482f249a4175c458c13ce03488bf
Ashish Vaid <ashish.vaid@soa.com>
2014-09-26 20:35:44 -0700

    enable static background with css3

--- js/default.js
+++ js/default.js
@@ -3,12 +3,14 @@
  */
 
 define(['jquery', 'bootstrap', 'fullpage', 'imagesloaded', 'easing'], function($){
-    console.log('Hi! Require is working!')
+
     $(document).ready(function() {
         $('#content').fullpage({
             verticalCentered: false,
-            easing: 'easeOutBack'
-
+            easing: 'easeOutBack',
+            navigation: true,
+            navigationPosition: 'right',
+            css3: true
         });
     });
 })
