26dd6e0a8c2b9dc61c44cd015af149dae58e9b42
Ashish Vaid <ashish.vaid@soa.com>
2014-09-29 17:41:48 -0700

    add down arrow to page 1

--- js/default.js
+++ js/default.js
@@ -2,15 +2,21 @@
  * Created by avaid on 9/8/14.
  */
 
-define(['jquery', 'bootstrap', 'fullpage', 'imagesloaded', 'easing'], function($){
+define(['jquery', 'bootstrap', 'fullpage', 'imagesloaded', 'easing'], function ($) {
 
-    $(document).ready(function() {
+    $(document).ready(function () {
         $('#content').fullpage({
             verticalCentered: true,
             easing: 'easeOutBack',
             navigation: true,
             navigationPosition: 'right',
             css3: true
+        });
+
+
+        $('#gotoSection2').click(function () {
+            $('#fp-nav li:nth-child(2) a').click();
+            return false;
         });
     });
 })
